b1da396c546e15ade167500b73564f04385dea92
Abhishek <abhishek@gmail.com>
2019-12-15 22:30:36 +0530

    ss

---
+++ java2-backend/src/test/java/java/AppTest.java
@@ -0,0 +1,12 @@
+package java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
